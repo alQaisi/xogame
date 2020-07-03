131c54cb28a00d87e06fa6a9caf6a66dc09ebc1d
ahmedhamed1312@gmail.com <ahmedhamed1312@gmail.com>
2020-07-03 17:19:02 +0300

    Updates

---
+++ precache-manifest.c721b441fd7fc3140b03f24e672289ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd8bd95976ca3935408de5beda1d7d2",
+    "url": "/xogame/index.html"
+  },
+  {
+    "revision": "999b8d85a854e8d57ce9",
+    "url": "/xogame/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bff8d8d18fa8c77f7eea",
+    "url": "/xogame/static/css/main.7ca81c76.chunk.css"
+  },
+  {
+    "revision": "999b8d85a854e8d57ce9",
+    "url": "/xogame/static/js/2.24986eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xogame/static/js/2.24986eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff8d8d18fa8c77f7eea",
+    "url": "/xogame/static/js/main.363ffe3d.chunk.js"
+  },
+  {
+    "revision": "5bc039fb841761591b1b",
+    "url": "/xogame/static/js/runtime-main.7eeb6c95.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/alQaisi/xogame/precache-manifest.7d42dc4edce69371ed7c870b14b277a2.js"
+  "/xogame/precache-manifest.c721b441fd7fc3140b03f24e672289ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alQaisi/xogame/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/xogame/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
