f0c47456c946eb9c5c32a7d476bcab4edd652322
ahmedhamed1312@gmail.com <ahmedhamed1312@gmail.com>
2020-07-03 17:04:46 +0300

    Updates

---
+++ precache-manifest.7d42dc4edce69371ed7c870b14b277a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4544187b180ee1676b858498ed656ec",
+    "url": "/alQaisi/xogame/index.html"
+  },
+  {
+    "revision": "8e3e5556fdac362d3358",
+    "url": "/alQaisi/xogame/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d5490f4472ebf7aba3fb",
+    "url": "/alQaisi/xogame/static/css/main.7ca81c76.chunk.css"
+  },
+  {
+    "revision": "8e3e5556fdac362d3358",
+    "url": "/alQaisi/xogame/static/js/2.20f91426.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alQaisi/xogame/static/js/2.20f91426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5490f4472ebf7aba3fb",
+    "url": "/alQaisi/xogame/static/js/main.d510847b.chunk.js"
+  },
+  {
+    "revision": "80ecc18222f7c04e20d9",
+    "url": "/alQaisi/xogame/static/js/runtime-main.2f88e0e9.js"
+  }
+]);
